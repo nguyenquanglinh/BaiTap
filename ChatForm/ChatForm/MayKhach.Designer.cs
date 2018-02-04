03320d01f21df8c57b67f5815ced91dcc8c0ec7d
quang <quang@gmail.com>
2018-02-04 22:14:08 +0700

    Làm lại thành công chat form

---
+++ ChatForm/ChatForm/MayKhach.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ChatForm
+{
+    partial class MayKhach
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.KhungChat = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(29, 91);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(13, 49);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // KhungChat
+            // 
+            this.KhungChat.Location = new System.Drawing.Point(131, 74);
+            this.KhungChat.Name = "KhungChat";
+            this.KhungChat.Size = new System.Drawing.Size(100, 96);
+            this.KhungChat.TabIndex = 3;
+            this.KhungChat.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(131, 212);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.Location = new System.Drawing.Point(147, 27);
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.Size = new System.Drawing.Size(100, 20);
+            this.txtReceive.TabIndex = 5;
+            this.txtReceive.Text = "*";
+            this.txtReceive.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(131, 176);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(100, 20);
+            this.txtMessage.TabIndex = 6;
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.Location = new System.Drawing.Point(29, 160);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSendFile.TabIndex = 7;
+            this.btnSendFile.Text = "SendFile";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            this.btnSendFile.Click += new System.EventHandler(this.btnSendFile_Click);
+            // 
+            // MayKhach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnSendFile);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.KhungChat);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "MayKhach";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.RichTextBox KhungChat;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtReceive;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSendFile;
+    }
+}
+
---
+++ ChatForm/ChatForm/MayKhach.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using ChatLib;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChatLib;
+using ChatLib.MessageModel;
+using System.IO;
+
+namespace ChatForm
+{
+    public partial class MayKhach : Form
+    {
+        ClientChat client;
+        public MayKhach()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            client = new ClientChat(2302);
+            client.Processor.Add(new SendProcess(this));
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            client.Send(new SendMessage(txtUserName.Text, txtReceive.Text, txtMessage.Text));
+            ThemCauChat(txtMessage.Text);
+        }
+
+
+
+
+
+
+        #region
+        class SendProcess : MessagerProcessorBase
+        {
+            MayKhach form;
+            //1 hàm tạo
+            public SendProcess(MayKhach form)
+            {
+                this.form = form;
+            }
+            //2 tên máy khách
+            public override void Process(ChatLib.MessageModel.ConnectMessageSuccess message)
+            {
+                this.form.Text = message.NameSender;
+            }
+            //3 gửi tin nhắn thất bại
+            public override void Process(ChatLib.MessageModel.ConnecMessageFaile message)
+            {
+                this.form.KhungChat.AppendText(message.Exception + "\n");
+            }
+            //5 nhận được file
+            public override void Process(ChatLib.MessageModel.RecievedFile message)
+            {
+                this.form.KhungChat.AppendText("Recieve from " + message.NameSender + " file name:" + message.FileName);
+                File.WriteAllBytes("d:/chat/" + message.FileName, message.ByteData);
+
+            }
+            //4 nhận được tin nhắn
+            public override void Process(ChatLib.MessageModel.RecievedMessage message)
+            {
+                this.form.KhungChat.AppendText(string.Format("{0}: {1} \n", message.Form, message.Message));
+            }
+
+
+        }
+        #endregion
+
+        private void btnSendFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var file = File.ReadAllBytes(open.FileName);
+                if (file.Length > 1024)
+                {
+                    var tepFile = ChuyenFileLon(file);
+                    client.Send(new SendFileBig(txtUserName.Text, txtReceive.Text, Path.GetFileName(open.FileName), tepFile.Count));
+                    for (int i = 0; i < tepFile.Count; i++)
+                    {
+                        client.Send(new SendFileBig(i.ToString(), tepFile[i]));
+                    }
+                }
+                else
+                {
+                    client.Send(new SenFile(txtUserName.Text, File.ReadAllBytes(open.FileName), txtReceive.Text, Path.GetFileName(open.FileName)));
+                    ThemCauChat("đã gửi file " + open.FileName);
+                }
+            }
+        }
+
+        private List<byte[]> ChuyenFileLon(byte[] file)
+        {
+            var result = new List<byte[]>();
+            for (int i = 0; i < file.Length; i += 1024)
+            {
+                result.Add(LayFileDuLon(i, file));
+            }
+            return result;
+        }
+
+        private byte[] LayFileDuLon(int p1, byte[] file)
+        {
+            var data = new byte[1024];
+            int x;
+            for (int i = 0; i < 1024; i++)
+            {
+                if (i + p1 < file.Length)
+                    data[i] = file[p1 + i];
+                else break;
+            }
+            return data;
+        }
+        private void ThemCauChat(string cauChat)
+        {
+            this.KhungChat.AppendText(cauChat + "\n");
+            txtMessage.Clear();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            client.Connect(txtUserName.Text);
+        }
+    }
+}
